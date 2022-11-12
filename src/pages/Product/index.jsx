afbc2e69bc0191777b7519c8a5382989fd9d84a4
sylvestre1024 <sylvestre1024@gmail.com>
2022-11-12 19:13:48 +0100

    Maj ajout slideshow

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { StyledLink } from '../../utils/style/Atoms'
+import Slideshow from "../../components/Layout/Slideshow"
 import Collapse from '../../components/Layout/Collapse'
 const productData = require("./logements.json")
 
@@ -24,16 +25,42 @@ function getProductWithId (productData, productId) {
     }
 }
 
-function Product() { 
-    const {productId} = useParams()
-    const product = getProductWithId(productData, productId)
-
-    return (
-      <div>test 
-        <div>id : {productId}</div>
-        <div>{product.title}</div>
-        <div>{product.location}</div>
-        </div>
+function Product() {
+  const { productId } = useParams()
+  const product = getProductWithId(productData, productId)
+  console.log(product)
+  return (   
+          <div>
+            <Slideshow photosArray={product.pictures} />
+            <div>
+              <div>
+                <div>Titre : {product.title}</div>
+                <div>Localisation : {product.location}</div>
+                <ul>
+                  {product.tags.map((tag)=>(
+                      <li key={tag.index}>{tag}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <div>
+                  <div>Qui : {product.host.name}</div>
+                  <div><img src={product.host.picture} alt={product.host.name} /></div>
+                </div>
+              </div>
+            </div>
+            <div>Note : {parseInt(product.rating)}</div>
+            <ul>Equipments :
+              {product.equipments.map((tag)=>(
+                <li key={tag.index}>{tag}</li>
+                    ))}
+            </ul>
+            <ul>
+              {product.pictures.map((tag)=>(
+                <li key={tag.index}>{tag}</li>
+                    ))}
+            </ul>
+          </div>
     )
 }
 
