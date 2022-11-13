6d140b19d4e82c505ed34030650843efc27de836
sylvestre1024 <sylvestre1024@gmail.com>
2022-11-13 14:39:13 +0100

    Ajout page 404

--- src/pages/Errors/index.jsx
+++ src/pages/Errors/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './styles.css'
+
+
+// ==============================================================
 
 function Errors() {
-  return (
-    <div>
-      <div>
-          &#10097;Page 404
-      </div>
-      <hr/>
-    </div>
-  )
+    return (
+        <div className='container_errors'>
+            <p className='errors_number'>404</p>
+            <h1 className='errors_text'>Oups! La page que vous demandez n'existe pas.</h1>
+            <Link to="/">
+                <p className='text_return'>Retourner sur la page d’accueil</p>
+            </Link>
+        </div>
+    )
 }
 
 export default Errors
