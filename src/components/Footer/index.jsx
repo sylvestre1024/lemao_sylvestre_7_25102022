c1b3a82e6ef64c135a94e817414874e4a30b959e
sylvestre1024 <sylvestre1024@gmail.com>
2022-11-13 13:29:46 +0100

    Ajout Footer et Nav

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import LogoWhite from '../../images/LogoWhite.svg'
+import './styles.css'
 
-function Footer({picture}) {
-  picture="Chemin du logo du Footer" // a remplacer par un import
-  return (
-    <div>Footer
-      <div><b>Chemin de l'image : </b>{picture}</div>
-      <hr/>
-    </div>
-  )
+// ==============================================================
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <img src={LogoWhite} alt='logo' className='LOGO' />
+            <p className='Text_footer'>© 2020 Kasa. All rights reserved</p>
+        </div>
+    )
 }
 
 export default Footer
-
-
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../../components/Header'
 import Banner from '../../components/Banner'
 import ListCard from '../../components/ListCard'
 
 function Home() {
     return (
         <div>
+            <Header />
             <Banner />
             <ListCard />
         </div>
